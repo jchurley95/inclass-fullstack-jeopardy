3094a59bc20cd75714b94c44d786b25d2d2f5a48
jchurley95 <jchurley95@gmail.com>
2017-08-09 14:53:10 -0400

    Added proxy server

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
+  constructor(){
+    super();
+    this.state = {
+      user: "",
+      redirect: false,
+      games: []
+    }
+  }
+
+  componentWillMount(){
+    axios.get('localhost:3001/api/game').then(res => {
+      console.log(res.data);
+      this.setState({games: res.data})
+    });
+  }
+
   render() {
     return (
       <div>
-        Hello from Home
+        <div>
+          <h1>What is your username?</h1>
+          <input type="text" name="user" />
+          <button>New Game</button>
+        </div>
+        {this.state.games.map((game, i) => (
+          <div key={i}>
+            <Link to={`/game/${game._id}`}>
+              {game.user}'s Game
+            </Link>
+          </div>
+        ))}
       </div>
     );
   }
